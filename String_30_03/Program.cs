2fc1ada95963ff080f324cdf0cbcab672936c263
InkaShev7777 <Sevc_ph43@student.itstep.ord>
2022-03-30 09:47:06 +0300

    1

---
+++ String_30_03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace String_30_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // замена всех слов короче 3 символов на secret
+
+
+            string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            string[] mas = text.Split(" ");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i].Length < 3)
+                {
+                    mas[i] = "SECRET";
+                }
+                Console.Write($"{mas[i]} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
