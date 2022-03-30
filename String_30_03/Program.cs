997139f72db5f640bfd1a6a3c5484e8a80b91e33
InkaShev7777 <Sevc_ph43@student.itstep.ord>
2022-03-30 11:41:19 +0300

    4,5

--- String_30_03/Program.cs
+++ String_30_03/Program.cs
@@ -47,20 +47,53 @@ namespace String_30_03
 
             //замена z,s,c на Uper
 
+            //string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            //string rez = "";
+            //rez = text.Replace("c", "C");
+            //text = rez;
+            //rez = text.Replace("s", "S");
+            //text = rez;
+            //text.Replace("z", "Z");
+            //text = rez;
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    Console.Write(text[i]);
+            //}
+
+
+            //все строки в параграфы
+
             string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
             string rez = "";
-           
-
-            rez = text.Replace("c", "C");
-            text = rez;
-            rez = text.Replace("s", "S");
-            text = rez;
-            text.Replace("z", "Z");
-            text = rez;
+            for(int i =0;i<text.Length;i++)
+            {
+                if(text[i] == '.')
+                {
+                    rez = text.Insert(i+1, "\n");
+                    text = rez;
+                }
+                
+            }
             for (int i = 0; i < text.Length; i++)
             {
                 Console.Write(text[i]);
             }
+                //заменить I на same
+
+
+                //string text = " Ilya I Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+                //string[] rez = text.Split(" ");
+                //for(int i = 0; i< rez.Length;i++)
+                //{
+                //    if (rez[i] == "I")
+                //    {
+                //        rez[i] = "same";
+                //    }
+                //}
+                //for (int i = 0; i < rez.Length; i++)
+                //{
+                //    Console.Write($"{rez[i]} ");
+                //}
                 Console.ReadKey();
         }
     }
