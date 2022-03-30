71b3604a376aaac929a0938b4fdd990586e76a8b
InkaShev7777 <Sevc_ph43@student.itstep.ord>
2022-03-30 10:49:25 +0300

    3

--- String_30_03/Program.cs
+++ String_30_03/Program.cs
@@ -24,25 +24,42 @@ namespace String_30_03
             //вывод слов по убыванию
 
 
+            //string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            //string[] mas = text.Split(" ");
+            //string zam;
+            //for(int i = 0;i<mas.Length;i++)
+            //{
+            //    for(int j = mas.Length-1;j>i;j--)
+            //    {
+            //        if (mas[j].Length > mas[j - 1].Length)
+            //        {
+            //            zam = mas[j];
+            //            mas[j] = mas[j - 1];
+            //            mas[j - 1] = zam;
+            //        }
+            //    }
+
+            //}
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    Console.WriteLine($"{mas[i]} ");
+            //}
+
+            //замена z,s,c на Uper
+
             string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            string[] mas = text.Split(" ");
-            string zam;
-            for(int i = 0;i<mas.Length;i++)
+            string rez = "";
+           
+
+            rez = text.Replace("c", "C");
+            text = rez;
+            rez = text.Replace("s", "S");
+            text = rez;
+            text.Replace("z", "Z");
+            text = rez;
+            for (int i = 0; i < text.Length; i++)
             {
-                for(int j = mas.Length-1;j>i;j--)
-                {
-                    if (mas[j].Length > mas[j - 1].Length)
-                    {
-                        zam = mas[j];
-                        mas[j] = mas[j - 1];
-                        mas[j - 1] = zam;
-                    }
-                }
-                
-            }
-            for (int i = 0; i < mas.Length; i++)
-            {
-                Console.WriteLine($"{mas[i]} ");
+                Console.Write(text[i]);
             }
                 Console.ReadKey();
         }
